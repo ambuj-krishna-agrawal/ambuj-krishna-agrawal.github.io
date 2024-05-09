7690f9f0e92149c3e83509595f603972cc7f78ca
Ambuj Agrawal <ambuj.agrawal@cred.club>
2024-05-09 05:57:09 +0530

    fixing headings

--- src/data/education.js
+++ src/data/education.js
@@ -39,27 +39,6 @@ function education_1() {
 	};
 }
 
-function education_2() {
-	return {
-		date: "7 May 2023",
-		title: "Artificial Intelligence in Healthcare",
-		description:
-			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
-		style: ``,
-		keywords: [
-			"Artificial Intelligence in Healthcare",
-			"Tharindu",
-			"Tharindu N",
-			"Tharindu Nayanajith",
-		],
-		body: (
-			<React.Fragment>
-				<h1>Content of article 2</h1>
-			</React.Fragment>
-		),
-	};
-}
-
-const myEducation = [education_1, education_2];
+const myEducation = [education_1];
 
 export default myEducation;
