bcdb1917706671fd77e2ff5a64d784742788a3e4
Ambuj Agrawal <ambuj.agrawal@cred.club>
2024-05-11 14:35:47 +0530

    app js routing fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ReactGA from "react-ga4";
 import Homepage from "./pages/homepage";
 import About from "./pages/about";
